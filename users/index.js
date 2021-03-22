02ab0e8ed75fead41389627b6bee58bc855fc7c4
nwa24 <nwa24@sfu.ca>
2021-03-22 15:11:53 -0700

    updated signup and signin

--- users/index.js
+++ users/index.js
@@ -5,22 +5,22 @@ const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 function userListClassrooms(ctx, studentId) {
-    const params = {
-        TableName: "Classroom",
-        ProjectionExpression: "course", // specifies the attributes you want in the results
-        FilterExpression: "contains (students, :studentId)", // returns only items that satisfy this condition
-        ExpressionAttributeValues: {
-            ":studentId": studentId
-        }
-    };
-    
-    // scan = reads every item in the table, maybe consider using a secondary index for query instead
-    return new Promise((resolve, reject) => {
-        docClient.scan(params, (error, data) => {
-            if (error) resolve({ statusCode: 400, error: error });
-            resolve({ statusCode: 200, body: JSON.stringify(data.Items) });
-        });
+  const params = {
+    TableName: "Classroom",
+    ProjectionExpression: "course", // specifies the attributes you want in the results
+    FilterExpression: "contains (students, :studentId)", // returns only items that satisfy this condition
+    ExpressionAttributeValues: {
+      ":studentId": studentId
+    }
+  };
+
+  // scan = reads every item in the table, maybe consider using a secondary index for query instead
+  return new Promise((resolve, reject) => {
+    docClient.scan(params, (error, data) => {
+      if (error) resolve({ statusCode: 400, error: error });
+      resolve({ statusCode: 200, body: JSON.stringify(data.Items) });
     });
+  });
 }
 
 function userSignup(ctx, user, password) {
@@ -34,48 +34,48 @@ function userSignup(ctx, user, password) {
                 firstName : user.firstName,
                 lastName : user.lastName,
                 role : user.role,
-                relatedUsers : user.relatedUsers
+                // relatedUsers : user.relatedUsers
             }
         },
     };
-    
-    return new Promise((resolve, reject) => {
-        docClient.put(params, (error, data) => {
-            if (error) {
-                resolve({
-                    statusCode: 400,
-                    error: error,
-                });
-            } else {
-                resolve({ statusCode: 200, body: JSON.stringify(params.Item) });
-            }
+
+  return new Promise((resolve, reject) => {
+    docClient.put(params, (error, data) => {
+      if (error) {
+        resolve({
+          statusCode: 400,
+          error: error,
         });
+      } else {
+        resolve({ statusCode: 200, body: JSON.stringify(params.Item) });
+      }
     });
+  });
 }
 
 function userLogin(ctx, email, password) {
     const params = {
         TableName: "User",
-        ProjectionExpression: "#user", // specifies the attributes you want in the results
+        ProjectionExpression: "#user, #id", // specifies the attributes you want in the results
         FilterExpression: "#user.email = :email AND #user.password = :password", // returns only items that satisfy this condition
         ExpressionAttributeValues: {
             ":email": email,
             ":password" : password
         },
         ExpressionAttributeNames: {
-            "#user": "user"
+            "#user": "user",
+            "#id": "id"
         }
     };
-  
-    // scan = reads every item in the table, maybe consider using a secondary index for query instead
-    return new Promise((resolve, reject) => {
-        docClient.scan(params, (error, data) => {
-            if (error) resolve({ statusCode: 400, error: error });
-            resolve({ statusCode: 200, body: JSON.stringify(data.Items) });
-        });
-    });
-}
 
+  // scan = reads every item in the table, maybe consider using a secondary index for query instead
+  return new Promise((resolve, reject) => {
+    docClient.scan(params, (error, data) => {
+      if (error) resolve({ statusCode: 400, error: error });
+      resolve({ statusCode: 200, body: JSON.stringify(data.Items[0]) });
+    });
+  });
+}
 
 exports.handler = new ServiceBuilder()
   .addInterface('userSignup', userSignup)
