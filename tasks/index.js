7920c644027185dc6693a16438c07dfb8e95c973
tabjy <admin@tabjy.com>
2021-04-04 22:16:38 -0700

    REFACTOR: tasks service

--- tasks/index.js
+++ tasks/index.js
@@ -1,98 +1,220 @@
-const { ServiceBuilder } = require('edu-monitor-sdk');
-const { v4: uuid } = require('uuid');
-const AWS = require('aws-sdk');
+const { promisify } = require('util')
 
-const docClient = new AWS.DynamoDB.DocumentClient();
+const AWS = require('aws-sdk')
+const { v4: uuid } = require('uuid')
+const Parameter = require('parameter')
 
-function addTask(ctx, classroom, title, deadline, student, done, addedBy) {
-  const params = {
-    TableName: 'Tasks',
-    Item: {
-      'id': uuid(),
-      'classroom': classroom,
-      'title': title,
-      'deadline': deadline,
-      'student': student,
-      'done': done,
-      'addedBy': addedBy,
-    },
-  };
+const { ServiceBuilder, Client } = require('edu-monitor-sdk')
 
-  return new Promise((resolve, reject) => {
-    docClient.put(params, (error, data) => {
-      if (error) reject(error);
-      resolve(JSON.stringify(params.Item));
-    });
-  });
+const LAMBDA_PREFIX = process.env['LAMBDA_PREFIX'] || 'EduMonitor_'
+
+const ROLES = {
+  STUDENT: 'STUDENT',
+  EDUCATOR: 'EDUCATOR',
+  GUARDIAN: 'GUARDIAN'
 }
 
-function deleteTask(ctx, taskId) {
-  const params = {
-    TableName: 'Tasks',
-    Key: {
-      'id': taskId,
-    },
-  };
-
-  return new Promise((resolve, reject) => {
-    docClient.delete(params, (error, data) => {
-      if (error) reject(error);
-      resolve(taskId);
-    });
-  });
+const RULES = {
+  title: {
+    type: 'string'
+  },
+  deadline: {
+    type: 'jsDate' // defined in `validate' function
+  }
 }
 
-function listTasks(ctx, studentId, classroomId) {
-  const params = {
-    TableName: 'Tasks',
-    ProjectionExpression: 'student, id, title, deadline, done, classroom, addedBy',
-    FilterExpression: 'student = :studentId',
-    ExpressionAttributesNames: {
-      '#student': 'student',
-    },
-    ExpressionAttributeValues: {
-      ':studentId': studentId,
-    },
-  };
+const db = new AWS.DynamoDB.DocumentClient()
+const TableName = LAMBDA_PREFIX + 'Tasks'
 
-  if (classroomId) {
-    params.FilterExpression = 'student = :studentId and classroom = :classroomId';
-    params.ExpressionAttributeValues[':classroomId'] = classroomId;
+const client = Client.create()
+
+function validate (document, requiredAll = true) {
+  const rules = {}
+  const data = {}
+  for (const key of Object.keys(document)) {
+    rules[key] = { ...RULES[key], required: requiredAll }
+    data[key] = document[key]
   }
 
-  return new Promise((resolve, reject) => {
-    docClient.scan(params, (error, data) => {
-      if (error) reject(error);
-      resolve(JSON.stringify(data.Items));
-    });
-  });
+  const validator = new Parameter()
+  validator.addRule('jsDate', (input) => isNaN(Date.parse(input)) ? 'should be a JavaScript date String' : undefined)
+  const errors = validator.validate(rules, data)
+  if (errors) {
+    throw new Error(errors[0].field + ' ' + errors[0].message)
+  }
 }
 
-function updateTask(ctx, id, classroom, title, deadline, student, done, addedBy) {
-    const params = {
-    TableName: 'Tasks',
-    Item: {
-      'id': id,
-      'classroom': classroom,
-      'title': title,
-      'deadline': deadline,
-      'student': student,
-      'done': done,
-      'addedBy': addedBy,
-    },
-  };
+class TaskService {
+  async lookup (context, id) {
+    if (!context.session.user) {
+      throw new Error('You are not logged in')
+    }
 
-  return new Promise((resolve, reject) => {
-    docClient.put(params, (error, data) => {
-      if (error) reject(error);
-      resolve(JSON.stringify(params.Item));
-    });
-  });
+    const fields = ['id', 'title', 'classroom', 'deadline', 'student', 'completedStudents']
+    const data = await promisify(db.scan).bind(db)({
+      TableName,
+      ProjectionExpression: fields.map(field => '#' + field).join(', '),
+      FilterExpression: '#id = :id',
+      ExpressionAttributeValues: {
+        ':id': id
+      },
+      ExpressionAttributeNames: Object.assign({}, ...fields.map(field => ({ ['#' + field]: field })))
+    })
+
+    return data.Items[0] || null // the classroom, or null
+  }
+
+  async create (context, { title, deadline, classroom }) {
+    if (!context.session.user) {
+      throw new Error('You are not logged in')
+    }
+
+    const theClassroom = await client.Classrooms.lookup(classroom)
+    if (!theClassroom) {
+      throw new Error('Classroom not found')
+    }
+
+    let student = null
+    if (context.session.user.role === ROLES.STUDENT) {
+      student = context.session.user.id
+    } else if (context.session.user.role === ROLES.EDUCATOR) {
+      if (theClassroom.instructor !== context.session.user.id) {
+        throw new Error('You are not the instructor of this classroom')
+      }
+    } else {
+      throw new Error('Unauthorized')
+    }
+
+    validate({ title, deadline })
+
+    const task = {
+      id: uuid(),
+      title,
+      classroom,
+      deadline,
+      student, // null: task is for the entire class, non-null: task is for that specific student only
+      completedStudents: []
+    }
+
+    await promisify(db.put).bind(db)({
+      TableName,
+      Item: task
+    })
+
+    return task
+  }
+
+  async update (context, id, { title, deadline }) {
+    const task = await this.lookup(context, id)
+    if (!task) {
+      throw new Error('Task not found')
+    }
+
+    const theClassroom = await client.Classrooms.lookup(classroom)
+    if (!theClassroom) {
+      throw new Error('Classroom not found')
+    }
+
+    if (task.student && task.student !== context.session.user.id) {
+      throw new Error('Unauthorized')
+    }
+
+    if (!task.student && theClassroom.instructor !== context.session.user.id) {
+      throw new Error('Unauthorized')
+    }
+
+    task.title = title || task.title
+    task.deadline = deadline || task.deadline
+
+    await promisify(db.put).bind(db)({
+      TableName,
+      Item: task
+    })
+
+    return task
+  }
+
+  async delete (context, id) {
+    const task = await this.lookup(context, id)
+    if (!task) {
+      throw new Error('Task not found')
+    }
+
+    const theClassroom = await client.Classrooms.lookup(classroom)
+    if (!theClassroom) {
+      throw new Error('Classroom not found')
+    }
+
+    if (task.student && task.student !== context.session.user.id) {
+      throw new Error('Unauthorized')
+    }
+
+    if (!task.student && theClassroom.instructor !== context.session.user.id) {
+      throw new Error('Unauthorized')
+    }
+
+    await promisify(db.delete).bind(db)({
+      TableName,
+      Key: {
+        'id': id
+      }
+    })
+
+    return null
+  }
+
+  async list (context, classroom) {
+    if (!context.session.user) {
+      throw new Error('You are not logged in')
+    }
+
+    const fields = ['id', 'title', 'classroom', 'deadline', 'student', 'completedStudents']
+    const data = await promisify(db.scan).bind(db)({
+      TableName,
+      ProjectionExpression: fields.map(field => '#' + field).join(', '),
+      FilterExpression: '#id = :id',
+      ExpressionAttributeValues: {
+        ':classroom': classroom
+      },
+      ExpressionAttributeNames: Object.assign({}, ...fields.map(field => ({ ['#' + field]: field })))
+    })
+
+    return data.Items.filter(task => task.student === null || task.student === context.session.user.id)
+  }
+
+  async updateCompleteness (context, id, completed) {
+    const task = await this.lookup(context, id)
+    if (!task) {
+      throw new Error('Task not found')
+    }
+
+    if (completed) {
+      if (task.completedStudents.indexOf(context.session.user.id) === -1) {
+        task.completedStudents.push(context.session.user.id)
+      }
+    } else {
+      if (task.completedStudents.indexOf(context.session.user.id) !== -1) {
+        task.completedStudents.splice(task.completedStudents.indexOf(context.session.user.id), 1)
+      }
+    }
+
+    await promisify(db.delete).bind(db)({
+      TableName,
+      Key: {
+        'id': id
+      }
+    })
+
+    return task
+  }
 }
 
+const tasks = new TaskService()
 exports.handler = new ServiceBuilder()
-  .addInterface('addTask', addTask)
-  .addInterface('deleteTask', deleteTask)
-  .addInterface('listTasks', listTasks)
-  .addInterface('updateTask', updateTask)
-  .build();
+  .addInterface('lookup', tasks.lookup, tasks)
+  .addInterface('create', tasks.create, tasks)
+  .addInterface('update', tasks.update, tasks)
+  .addInterface('delete', tasks.delete, tasks)
+  .addInterface('list', tasks.list, tasks)
+  .addInterface('updateCompleteness', tasks.updateCompleteness, tasks)
+  .build()
